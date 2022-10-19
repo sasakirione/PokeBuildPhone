1303dd3b67459c6615bbfdb8c2807871a5a4ef18
sasakirione <sasakirione@gmail.com>
2022-10-19 22:26:59 +0900

    Initial Commit

---
+++ PokeBuildPhone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeBuildPhone
+//
+//  Created by yamada on R 4/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeBuildPhone/PokeBuildPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeBuildPhoneApp.swift
+//  PokeBuildPhone
+//
+//  Created by yamada on R 4/10/19.
+//
+
+import SwiftUI
+
+@main
+struct PokeBuildPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
